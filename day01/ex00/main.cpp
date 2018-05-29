9f0b15f5b6d93767d01fcd1b4b7957de90430b5a
Lorette PRUNIER <lprunier@e1r8p13.42.fr>
2018-05-29 10:42:25 +0200

    d00 ex01 ok

--- day01/ex00/main.cpp
+++ day01/ex00/main.cpp
@@ -36,23 +36,23 @@ std::cout << "                                                         ##" << st
 }
 
 void    ponyOnTheHeap(void) {
-    std::cout << "Calling ponyOnTheHeap()" << std::endl;
     Pony    *pony = new Pony("Found", "Heap", "Ryan Moore", 3);
     draw();
     delete pony;
-	std::cout << "Returned from ponyOnTheHeap()" << std::endl;
 }
 
 void    ponyOnTheStack(void) {
-    std::cout << "Calling ponyOnTheStack()" << std::endl;
     Pony    pony("Nashwan", "Stack", "Willie Carson", 5);
     draw();
-    std::cout << "Returned from ponyOnTheStack()" << std::endl;    
 }
 
 int     main(void) {
-    
+
+    std::cout << "Calling ponyOnTheHeap()" << std::endl;
 	ponyOnTheHeap();
+	std::cout << "Returned from ponyOnTheHeap()" << std::endl;
+    std::cout << "Calling ponyOnTheStack()" << std::endl;
 	ponyOnTheStack();
+    std::cout << "Returned from ponyOnTheStack()" << std::endl;
     return 0;
 }
