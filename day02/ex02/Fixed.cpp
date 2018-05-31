5316f53927dbcf8dd9c508f745f7f82418ee4efd
Lorette PRUNIER <lprunier@e1r8p13.42.fr>
2018-05-31 09:11:23 +0200

    d02 ex02 ok

--- day02/ex02/Fixed.cpp
+++ day02/ex02/Fixed.cpp
@@ -114,6 +114,34 @@ bool   Fixed::operator!=(Fixed const &copy) const {
     return (this->_value != copy.getRawBits());
 }
 
+Fixed &Fixed::min(Fixed & a, Fixed & b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+
+Fixed const 	&Fixed::min(Fixed const & a, Fixed const & b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+
+Fixed &Fixed::max(Fixed & a, Fixed & b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+Fixed const 	&Fixed::max(Fixed const & a, Fixed const & b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
 /////////////////////////////////////////////////////////////////////
 
 
--- day02/ex02/Fixed.hpp
+++ day02/ex02/Fixed.hpp
@@ -46,7 +46,12 @@ public:
     int     getRawBits(void) const;
     void    setRawBits(int const raw);
     float   toFloat(void) const;
-    int   toInt(void) const;
+    int     toInt(void) const;
+
+    static Fixed &min(Fixed & a, Fixed & b);
+	static Fixed const 	& min(Fixed const & a, Fixed const & b);
+	static Fixed &max(Fixed & a, Fixed & b);
+	static Fixed const 	& max(Fixed const & a, Fixed const & b);
 
 private:
     int                 _value;
